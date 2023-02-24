941456d0fad27558e3aa9be013de99eb47aced0c
ArshM17 <arshmaknojia5@gmail.com>
2023-02-24 16:55:42 +0530

    added game over screen

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ var size = 0;
 let div;
 var tempx;
 var tempy;
+var stat = 1;
 
 function setup(){
     div = createDiv('').size(100, 100);
@@ -30,40 +31,55 @@ function setup(){
 }
 
 function draw(){
-    background(0);
-    square(appleX,appleY,side);
-    fill(0,255,0);
-    tempx = x;
-    tempy = y;
-    x+=vx;
-    y+=vy;
-    square(x,y,side);
-    if(checkBite()){
+    if(stat==0){
+        //console.log("finish")
+        //background(0);
+        // div = createDiv('').size(100, 100);
+        // div.html("Game Over!"+"\n"+"Final Score:"+count);
+        textSize(32);
+        text('Game Over!', width/2-100, height/2);
+        fill(0, 102, 153);
+        text('Final score:'+count, width/2-100, height/2+50);
+        fill(0, 102, 153);
         noLoop();
-    }
-    if(tail.length>0){
-        for(let i = tail.length-1;i>0;i--){
-            tail[i][0] = tail[i-1][0];
-            tail[i][1] = tail[i-1][1];
-        }
-        tail[0][0] = tempx;
-        tail[0][1] = tempy;
-    }
-    for(let i = 0;i<tail.length;i++){
+    }else{
+        background(0);
+        square(appleX,appleY,side);
         fill(0,255,0);
-        square(tail[i][0],tail[i][1],side);
+        tempx = x;
+        tempy = y;
+        x+=vx;
+        y+=vy;
+        square(x,y,side);
+        if(checkBite()){
+            stat = 0;
+            //noLoop();
+        }
+        if(tail.length>0){
+            for(let i = tail.length-1;i>0;i--){
+                tail[i][0] = tail[i-1][0];
+                tail[i][1] = tail[i-1][1];
+            }
+            tail[0][0] = tempx;
+            tail[0][1] = tempy;
+        }
+        for(let i = 0;i<tail.length;i++){
+            fill(0,255,0);
+            square(tail[i][0],tail[i][1],side);
+        }
+        fill(255,0,0);
+        if(x>width-side || x<0 || y<0 || y>height-side){
+            stat = 0;
+            //noLoop();
+        }
+        if(x==appleX && y==appleY){
+            eat();
+        }
+        // x2=x;
+        // y2=y;
+        // x+=vx;
+        // y+=vy;
     }
-    fill(255,0,0);
-    if(x>width-side || x<0 || y<0 || y>height-side){
-        noLoop();
-    }
-    if(x==appleX && y==appleY){
-        eat();
-    }
-    // x2=x;
-    // y2=y;
-    // x+=vx;
-    // y+=vy;
 }
 
 function eat(){
